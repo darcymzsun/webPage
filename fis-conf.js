7b812ed33152997452bc09197c72435d45cfbf8d
sunmingzhang <sunmingzhang@zuoyebang.com>
2016-03-30 19:53:55 +0800

    背景

---
+++ fis-conf.js
@@ -0,0 +1,51 @@
+//开启相对路径
+fis.hook('relative'); 
+
+//less编译
+fis.match('**.less', {
+    parser: fis.plugin('less'), // invoke `fis-parser-less`,
+    rExt: '.css',
+    useHash: true
+});
+
+// 加 md5
+fis.match('*.{js,css,png}', {
+  useHash: true
+});
+
+// 启用 fis-spriter-csssprites 插件
+fis.match('::package', {
+  spriter: fis.plugin('csssprites')
+})
+
+// 对 CSS 进行图片合并
+fis.match('*.css', {
+  // 给匹配到的文件分配属性 `useSprite`
+  useSprite: true,
+  release: './static/css$0'
+});
+
+fis.match('*.js', {
+  // fis-optimizer-uglify-js 插件进行压缩，已内置
+  optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('*.css', {
+  // fis-optimizer-clean-css 插件进行压缩，已内置
+  optimizer: fis.plugin('clean-css')
+});
+
+fis.match('*.png', {
+  // fis-optimizer-png-compressor 插件进行压缩，已内置
+  optimizer: fis.plugin('png-compressor')
+});
+
+fis.media('debug').match('*.{js,css,png}', {
+  useHash: false,
+  useSprite: false,
+  optimizer: null
+})
+//使用相对路径
+fis.match('**', { relative: true })
+
+
